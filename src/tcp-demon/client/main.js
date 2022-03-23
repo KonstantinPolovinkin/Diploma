90530eb8da1b586c912668b98634c8e63b531687
KonstantinPolovinkin <kostyaman555@gmail.com>
2022-03-23 21:57:04 +0200

    used electron/builder, refactored to one main.js file

--- src/tcp-demon/client/main.js
+++ src/tcp-demon/client/main.js
@@ -1,8 +1,58 @@
-const schedule = require('node-schedule');
-const cp = require('child_process');
+'use strict';
 
-(function () {
-  const job = schedule.scheduleJob('35 * * * * *', function(){
-    const n = cp.fork('./getHW.js');
+const { app } = require('electron');
+const schedule = require('node-schedule');
+const si = require('systeminformation');
+const fs = require('fs');
+
+const hwInfo = [];
+const siMethods = [
+  'system',
+  'bios',
+  'baseboard',
+  'chassis',
+  'cpu',
+  'mem',
+  'graphics',
+  'osInfo',
+  'diskLayout',
+];
+
+function fillArray() {
+  siMethods.forEach((method) => {
+    try {
+      si[`${method}`]().then(data => hwInfo.push(data));
+    } catch(err) {
+      console.log(err);
+    }
   });
-})();
+}
+
+function writeFile(data) {
+  fs.writeFile('./hwInfo.json', data, (err) => {
+    if (err)
+    console.log(err);
+  });
+}
+
+function main() {
+  fillArray();
+  writeFile('');
+
+  setTimeout(() =>
+    writeFile(JSON.stringify(hwInfo)),
+    20000);
+    
+  // doesn't work, writes an empty array
+  // setTimeout(() => 
+  //   app.quit(),
+  //   20000);
+}
+
+app.whenReady().then(() => {
+  (function () {
+    const scheduledJob = schedule.scheduleJob('42 * * * * *', function(){
+      main();
+    });
+  })();
+})
