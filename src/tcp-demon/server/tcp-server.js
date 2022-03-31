bb8236b961eea44eb97ac2e7318fb618940c778e
KonstantinPolovinkin <kostyaman555@gmail.com>
2022-03-31 23:38:15 +0300

    file naming changed, added event socket.on('end')

--- src/tcp-demon/server/tcp-server.js
+++ src/tcp-demon/server/tcp-server.js
@@ -3,18 +3,25 @@
 import net from 'net';
 import fs from 'fs';
 
-const onData = (data, ...args) => {
-  console.dir({ args });
-  let hwinfo = data.toString('utf8');
-  fs.writeFile('hwinfo.json', hwinfo, {encoding: 'utf8'}, (err) => {
+let address = String;
+
+const onData = (data) => {
+  const hwinfo = data.toString('utf8');
+  fs.writeFile(`${address}.json`, hwinfo, {encoding: 'utf8'}, (err) => {
     if (err)
       console.log(err);
   });
 };
 
 net.createServer(socket => {
+  address = socket.address().address.replace(/:/g, '-');
+  console.log('client connected');
   console.dir(socket.address());
-  socket.setNoDelay(true);
+
+  socket.setNoDelay(false);
   socket.write('server online');
   socket.on('data', onData);
+  socket.on('end', () => {
+    console.log(`client ${socket.address().address} disconnected`);
+  });
 }).listen(2000);
