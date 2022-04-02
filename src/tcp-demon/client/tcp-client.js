0f31d5c297ac9cd4636bb8fee5b1b87a63ed9e21
KonstantinPolovinkin <kostyaman555@gmail.com>
2022-04-02 04:30:38 +0300

    using argon2 for verification

--- src/tcp-demon/client/tcp-client.js
+++ src/tcp-demon/client/tcp-client.js
@@ -6,15 +6,20 @@ import fs from 'fs';
 export function sendFileToServer() {
   const socket = new net.Socket();
 
-  socket.on('data', data => {
-    console.log(data.toString('utf8'));
-  });
-
   socket.connect({
     port: 2000,
     host: '44.203.146.186',
   }, () => {
-    socket.write(fs.readFileSync('./hwinfo.json'));
-    socket.end();
+    socket.write('password SX*G6.Kpe8wch&i');
+    socket.on('data', data => {
+      console.log(data.toString('utf8'));
+
+      if (data.toString('utf8') === 'ok') {
+        socket.write(fs.readFileSync('./hwinfo.json'));
+        socket.end();
+      } else {
+        socket.end();
+      }
+    });
   });
 };
--- src/tcp-demon/server/tcp-server.js
+++ src/tcp-demon/server/tcp-server.js
@@ -1,7 +1,35 @@
 'use strict';
 
+import argon2 from 'argon2';
 import net from 'net';
 import fs from 'fs';
+
+async function verifyPassword(CURRENT_DEMON_INSTANCE_PASSWORD) {
+  const hwInfoFullPath = '200670621308384';
+
+  if (!fs.existsSync(`./${hwInfoFullPath}/hash.json`)) {
+    try {
+      const hash = await argon2.hash(CURRENT_DEMON_INSTANCE_PASSWORD);
+      fs.writeFileSync(`./${hwInfoFullPath}/hash.json`, JSON.stringify(hash));
+      return 1;
+    } catch(err) {
+      console.log(err);
+    }  
+    
+  } else {  
+    try {      
+      if (await argon2.verify(JSON.parse(fs.readFileSync(`./${hwInfoFullPath}/hash.json`)), CURRENT_DEMON_INSTANCE_PASSWORD)) {
+        console.log('ok');
+        return 1;
+      } else {
+        console.log('not ok');
+        return 0;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
 
 async function CreateStorageDir(HWstorageDirectory) {
   if (!fs.existsSync(HWstorageDirectory)) {
@@ -10,9 +38,9 @@ async function CreateStorageDir(HWstorageDirectory) {
 }
 
 async function getBaseBoardSerial(data) {
-  const baseBoardSerial = JSON.parse(data)[2].serial;
-  const HWstorageDirectory = `./${baseBoardSerial}`;
-  const hwInfoFullPath = `./${HWstorageDirectory}/${baseBoardSerial}.json`;
+    const baseBoardSerial = JSON.parse(data)[2].serial;
+    const HWstorageDirectory = `./${baseBoardSerial}`;
+    const hwInfoFullPath = `./${HWstorageDirectory}/${baseBoardSerial}.json`;
 
   await CreateStorageDir(HWstorageDirectory);
   WriteLocalFile(data, hwInfoFullPath);
@@ -26,17 +54,24 @@ function WriteLocalFile(data, hwInfoFullPath) {
   });
 }
 
-const onData = async (data) => {
-  getBaseBoardSerial(data);
-};
-
 net.createServer(socket => {
   console.log('client connected');
   console.dir(socket.address());
 
   socket.setNoDelay(false);
-  socket.write('server online');
-  socket.on('data', onData);
+  socket.on('data', async (data) => {
+    const isItPassword = data.toString('utf8').startsWith('password');
+    if (isItPassword) {
+      const getPassword = data.toString('utf8').slice(9);
+      if (await verifyPassword(getPassword)) {
+        socket.write('ok');
+      } else {
+        socket.write('not ok');
+      }
+    } else {
+      getBaseBoardSerial(data);
+    }
+  });
   socket.on('end', () => {
     console.log(`client ${socket.address().address} disconnected`);
   });
